db05bee24d0b6a6d4560f5e82870d8aed136f5a7
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-12-22 19:38:27 +0100

    send data via props from file

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
 import Note from "./components/note";
+import DATA from "./notes.json";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {DATA.map((val, key) => {
+        return <Note key={val.key} title={val.title} content={val.content} />;
+      })}
       <Footer />
     </div>
   );
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Note = () => {
+const Note = (props) => {
   return (
     <div className="note">
-      <h1>Continue using keeper</h1>
-      <p>This is the description of the note</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
