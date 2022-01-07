21f97fb01c965eaa3b56c3184a89e0672da2c259
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-07 11:40:56 +0100

    handle change and state for new notes

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateArea() {
+  const [input, setInput] = useState({
+    title: "",
+    note: ""
+  });
+
+  function handleChange(event) {
+    let { value, name } = event.target;
+    if ("title" === name) {
+      setInput({
+        title: value,
+        note: input.note
+      });
+    } else {
+      setInput({
+        title: input.title,
+        note: value
+      });
+    }
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleChange}
+          value={input.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          value={input.note}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
