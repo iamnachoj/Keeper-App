80ce26562f720a455d7cd297d23fd00680a76aff
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-07 11:23:41 +0100

    add CreateArea component, and CSS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
+import CreateArea from "./components/create.jsx";
 import Note from "./components/note";
 import DATA from "./notes.json";
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div>
       <Header />
+      <CreateArea />
       {DATA.map((val, key) => {
         return <Note key={val.key} title={val.title} content={val.content} />;
       })}
